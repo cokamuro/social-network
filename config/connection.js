52c15e40a1eb9ee25308f54a3564b7ae4dfd679f
Chris Okamuro <cokamuro@gmail.com>
2022-07-18 10:09:19 -0400

    created basic file and folder structure.  added config files and packages

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+
+const { connect, connection } = require('mongoose');
+
+connect('mongodb://127.0.0.1:27017/socialnetwork_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
