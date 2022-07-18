3ff084160977fc03b71d03ddda05a8ea473d8b35
Chris Okamuro <cokamuro@gmail.com>
2022-07-18 10:27:09 -0400

    organized user routes by ones using a userId

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,12 @@
+// api/thoughts
+
+// get all thoughts
+// get a single thought by id
+// post to create a new thought - also put the thought on the user's thoughts array field
+// put to update thought by id
+// delete to remove thought by id
+
+// api/thoughts/:thoughtId/reactions
+
+// post to create a reaction stored in a single thought's reactions array
+// delete to pull and remove a reaction by the reactionId value
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+// api/users
+
+// get all users
+// post a new user
+
+// api/users/:userId
+
+// get single user by id, populated thought and friend data
+// put to update a user by id
+// delete to remove user by id
+// bonus: remove a user's associated thoughts when deleted
+
+// api/users/:userId/friends/:friendId
+
+// post to add a new friend to a user's friend list
+// delete to remove a friend from a user's friend list
